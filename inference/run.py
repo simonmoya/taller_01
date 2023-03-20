9879b8bf1744f34d7aacab7c479f9710176c932d
SimonMoyaJ <simonmoya@gmail.com>
2023-03-19 21:09:55 -0500

    Configuración de swagger en run.py

--- inference/run.py
+++ inference/run.py
@@ -8,9 +8,7 @@ import json
 from sklearn.ensemble import RandomForestClassifier
 from flasgger import Swagger
 from flasgger.utils import swag_from
-from apispec import APISpec
-from apispec.ext.marshmallow import MarshmallowPlugin
-from apispec_webframeworks.flask import FlaskPlugin
+from flasgger import LazyString, LazyJSONEncoder
 
 app = Flask(__name__)
 app.config['SECRET_KEY']="1234"
@@ -19,13 +17,28 @@ app.config["SWAGGER"] = {"title": "Swagger-UI", "uiversion": 2}
 swagger_config = {
     "swagger_version": "2.0",
     "title": "API de predicción de pingüinos",
+    "headers": [],
+    "specs": [
+        {
+            "endpoint": "apispec_1",
+            "route": "/apispec_1.json",
+            "rule_filter": lambda rule: True,  # all in
+            "model_filter": lambda tag: True,  # all in
+        }
+    ],
+    "static_url_path": "/flasgger_static",
     "swagger_ui": True,
-    "specs_route": "/docs/",
+    "specs_route": "/swagger/",
     "description": "Esta es una API para predecir la especie de pingüino en base a sus características.",
 }
 
 CORS(app)
-swagger = Swagger(app, config=swagger_config)
+template = dict(
+    swaggerUiPrefix=LazyString(lambda: request.environ.get("HTTP_X_SCRIPT_NAME", ""))
+)
+
+app.json_encoder = LazyJSONEncoder
+swagger = Swagger(app, config=swagger_config, template=template)
    
 @app.route("/inference", methods=['GET', 'POST'])
 @swag_from('pinguino.yml')
