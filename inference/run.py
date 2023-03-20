3f1ef81be3e708531dc4ceaf1bf516ce45e71c8b
SimonMoyaJ <simonmoya@gmail.com>
2023-03-19 21:03:41 -0500

    Configuración de swagger en run.py

--- inference/run.py
+++ inference/run.py
@@ -14,7 +14,18 @@ from apispec_webframeworks.flask import FlaskPlugin
 
 app = Flask(__name__)
 app.config['SECRET_KEY']="1234"
+app.config["SWAGGER"] = {"title": "Swagger-UI", "uiversion": 2}
+
+swagger_config = {
+    "swagger_version": "2.0",
+    "title": "API de predicción de pingüinos",
+    "swagger_ui": True,
+    "specs_route": "/docs/",
+    "description": "Esta es una API para predecir la especie de pingüino en base a sus características.",
+}
+
 CORS(app)
+swagger = Swagger(app, config=swagger_config)
    
 @app.route("/inference", methods=['GET', 'POST'])
 @swag_from('pinguino.yml')
